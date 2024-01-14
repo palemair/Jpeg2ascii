3f8fb2d29820dc8c605afdbb075901ba234c8fe5
palemair <palemair@gmail.com>
2024-01-14 18:52:49 +0100

    Création du dépot

---
+++ jpegutil.c
@@ -0,0 +1,76 @@
+/*
+ * =====================================================================================
+ *
+ *       Filename:  image.c
+ *
+ *    Description:  utilitaire images 
+ *
+ *        Version:  1.0
+ *        Created:  20/12/2023 21:10:24
+ *       Revision:  none
+ *       Compiler:  gcc
+ *
+ *         Author:  patricE (), 
+ *   Organization:  
+ *
+ * =====================================================================================
+ */
+#include "jpegutil.h"
+
+int readjpeg(char *filename, struct imgdata * ptimg) 
+{
+    struct jpeg_decompress_struct cinfo;
+    struct jpeg_error_mgr jerr;
+    FILE *infile;
+    JSAMPARRAY buffer = NULL;		/* Output row buffer */
+    unsigned int w,h,c,row_stride;	
+    size_t counter =0;
+    
+    cinfo.err = jpeg_std_error(&jerr);
+    jpeg_create_decompress(&cinfo);
+
+    if ((infile = fopen(filename, "rb")) == NULL)
+        {
+            perror("fopen");
+            jpeg_destroy_decompress(&cinfo);
+            return(EXIT_FAILURE);
+        }
+    jpeg_stdio_src(&cinfo, infile);
+    jpeg_read_header(&cinfo, TRUE);
+    jpeg_start_decompress(&cinfo);
+    w=cinfo.output_width;
+    h=cinfo.output_height;
+    c=cinfo.output_components;
+    row_stride = w * c;
+
+    buffer = (*cinfo.mem->alloc_sarray) ((j_common_ptr) &cinfo, JPOOL_IMAGE, row_stride, 1);
+
+    unsigned char *dest = malloc(row_stride * h * sizeof(unsigned char));
+
+    if(dest == NULL)
+    {
+        perror("Image malloc");
+        jpeg_destroy_decompress(&cinfo);
+        fclose(infile);
+        return(EXIT_FAILURE);
+    }
+
+    while (cinfo.output_scanline < h)
+    {
+        jpeg_read_scanlines(&cinfo, buffer, 1);
+        /* Assume put_scanline_someplace wants a pointer and sample count. */
+        memcpy(dest + counter, buffer[0], row_stride);
+        counter += row_stride;
+    }
+    
+    ptimg->data = dest;
+    ptimg->w = w;
+    ptimg->h = h;
+    ptimg->c = c;
+
+    jpeg_finish_decompress(&cinfo);
+    jpeg_destroy_decompress(&cinfo);
+    fclose(infile);
+   
+    return (EXIT_SUCCESS);
+}
---
+++ main.c
@@ -0,0 +1,168 @@
+/* programme jpeg to ascii */
+
+#include <stdlib.h>
+#include <string.h>
+#include <stdio.h>
+#include <math.h>
+#include <errno.h>
+#include <curses.h>
+#include <unistd.h>
+#include <term.h>
+#include "jpegutil.h"
+
+char liste[2][10] = { {'.', ',', '-', '~', 'c', 'r', '/', 'x', '=', '#'},
+{'.', '~', 'x', '/', '*', 'u', '|', '>', '<', 'X'}
+};
+
+int g = 0;
+
+void
+printimg (unsigned h, unsigned w, float t[h][w])
+{
+
+  for (unsigned int y = 0; y < h; y++)
+    {
+      for (unsigned int x = 0; x < w; x++)
+	{
+	  g = (int) (t[y][x] * 10);
+	  printf ("%c", liste[0][g]);
+	}
+      printf ("\n");
+    }
+}
+
+
+char filename[50];
+size_t counter = 0;
+unsigned int nblin, nbcol;
+
+
+int
+main (int argc, char **argv)
+{
+
+  switch (argc)
+    {
+    case 2:
+      {
+	strcpy (filename, argv[1]);
+	nbcol = 110;
+	nblin = 40;
+	break;
+      }
+
+    case 4:
+      {
+	strcpy (filename, argv[1]);
+	nbcol = atoi (argv[2]);
+	nblin = atoi (argv[3]);
+	break;
+      }
+
+    default:
+      {
+	strcpy (filename, "/usr/local/src/JPEG/img/House.jpg");
+	nbcol = 110;
+	nblin = 40;
+	break;
+      }
+    }
+
+  int ret = setupterm (NULL, STDOUT_FILENO, NULL);
+  unsigned int colterm;
+
+  if (ret == 0)
+    {
+      colterm = tigetnum ("cols");
+    }
+  else
+    {
+      colterm = 0;
+      printf ("pas de longueur de lignes et de colonnes");
+    }
+  (void) colterm;
+
+  /* image data */
+
+  struct imgdata photo;
+
+  if (readjpeg (filename, &photo) != EXIT_SUCCESS)
+    {
+      perror ("readjpeg");
+      exit (EXIT_FAILURE);
+    }
+
+  unsigned int h, w, c, dw, dh, modw, modh;
+  h = photo.h;
+  w = photo.w;
+  c = photo.c;
+  modw = w % nbcol;
+  modh = h % nblin;
+  dw = w / nbcol;
+  dh = h / nblin;
+
+  printf ("%d %d %d\n", w, h, c);
+  printf ("%d %d \n", dw, dh);
+  printf ("%d %d \n", modw, modh);
+
+  float (*pdata)[w] = malloc (sizeof (float[h][w]));
+
+  if (pdata == NULL)
+    {
+      perror ("malloc pdata");
+      exit (EXIT_FAILURE);
+    }
+
+  float buffer = 0.0;
+  unsigned char *ptimg = photo.data;
+
+  for (unsigned int y = 0; y < h; y++)
+    {
+      for (unsigned int x = 0; x < w; x++)
+	{
+	  buffer =
+	    (0.2126 * (*ptimg)) + (0.7152 * (*(ptimg + 1))) +
+	    (0.0722 * (*(ptimg + 2)));
+	  buffer /= 255;
+	  ptimg += 3;
+	  pdata[y][x] = buffer;
+	  buffer = 0.0;
+	}
+    }
+
+  free (photo.data);
+  photo.data = NULL;
+
+  float image[nblin][nbcol];
+
+  memset (image, 0.0, sizeof (float[nblin][nbcol]));
+
+  unsigned int u = 0;
+  unsigned int v = 0;
+  float moyenne = 0.0;
+
+  for (unsigned int x = 0; x < h - (modh + 1); x += dh)
+    {
+      for (unsigned y = 0; y < w - (modw + 1); y += dw)
+	    {
+	    for (unsigned int s = 0; s < dh; s++)
+	        {
+	        for (unsigned int t = 0; t < dw; t++)
+		        {
+		            moyenne += pdata[x + s][y + t];
+		        }
+	         }
+	        image[u][v] = moyenne / (dw * dh);
+	        moyenne = 0.0;
+	        v++;
+	    }
+      u++;
+      v = 0;
+    }
+
+  free (pdata);
+
+  printimg (nblin, nbcol, image);
+
+  return 0;
+}
